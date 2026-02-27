adcc55b131fb863575e8c310a0e2f7b4ca1b3b9a
Murilo Carvalho de Lucena <01009581245@semob.df.gov.br>
2026-02-27 09:28:48 -0300

    fix: configurando importação nos arquivos de configuração

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -63,6 +64,11 @@ export default defineConfig({
       }
     })
   ],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+    }
+  },
   optimizeDeps : {
     include: ['react-safe-area-component'],
   },
