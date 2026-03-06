a4d5e807e045594c97c253add9ab3bbc8e4269ef
Murilo Carvalho de Lucena <01009581245@semob.df.gov.br>
2026-03-06 11:40:44 -0300

    subindo novamente

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   preview: {
     proxy: {
       '/api-semob': {
-        target: 'http://dadoshlog.semob.gdfnet.df:8080/pcp',
+        target: 'https://2d3b-131-72-222-138.ngrok-free.app',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api-semob/, '')
@@ -81,7 +81,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api-semob': {
-        target: 'http://dadoshlog.semob.gdfnet.df:8080/pcp',
+        target: 'https://2d3b-131-72-222-138.ngrok-free.app',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api-semob/, '')
